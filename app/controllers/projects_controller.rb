d36ccb0469e8d89c53c24c1793dde8ed5b4c1743
LilyCole <Lilia.Cole@gmail.com>
2017-04-17 17:02:08 -0700

    adds devise for users, protects new, edit, update create routes

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,8 +1,8 @@
 class ProjectsController < ApplicationController
   before_action :get_project, only: [:show, :edit, :update, :destroy]
   before_action :get_images, only: [:show, :edit, :update, :destroy]
-  # before_action :verify_logged_in, only: [:create, :update,:destroy]
-  # before_action :verify_user, only: [:edit, :update, :destroy]
+  before_action :verify_logged_in, only: [:edit, :create, :update,:destroy]
+  before_action :verify_user, only: [:edit, :update, :destroy]
 
   def index
     @projects = Project.all.reverse_order
@@ -65,21 +65,21 @@ class ProjectsController < ApplicationController
     @images = Image.where(:project_id => params[:id])
   end
 
-  # def verify_logged_in
-  #   unless user_signed_in?
-  #     flash[:error] = 'You must be logged in.'
-  #     redirect_to new_user_session_path
-  #   end
-  # end
+  def verify_logged_in
+    unless user_signed_in?
+      flash[:error] = 'You must be logged in.'
+      redirect_to new_user_session_path
+    end
+  end
 
-  # def verify_user
-  #   unless params[:project] && params[:project][:flagged] || params[:action] != "destroy"
-  #     unless @project.owner == current_user || current_user.admin
-  #       flash[:error] = 'You are not authorized to perform this action.'
-  #       redirect_to :back
-  #     end
-  #   end
-  # end
+  def verify_user
+    unless params[:project] && params[:project][:flagged] || params[:action] != "destroy"
+      unless @project.owner == current_user || current_user.admin
+        flash[:error] = 'You are not authorized to perform this action.'
+        redirect_to :back
+      end
+    end
+  end
 
   def project_params
     params.require(:project).permit(:title,:summary)
