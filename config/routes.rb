f08be45359ce65fcb1d538bbc8ebc5cce71ca833
LilyCole <Lilia.Cole@gmail.com>
2017-04-17 18:27:38 -0700

    updates edit_registration form

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   
-  devise_for :users, controllers: { sessions: 'users/sessions' }
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+  }
   
   resources :projects do
     resources :images
